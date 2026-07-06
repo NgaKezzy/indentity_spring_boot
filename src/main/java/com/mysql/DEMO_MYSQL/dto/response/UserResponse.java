679e427da224b2fd98c0d881ae99f32ab1ef9694
Nga Nv <nganv.os@ncb-bank.vn>
2026-07-06 09:53:16 +0700

    feat: add JsonInclude non-null constraint to UserResponse DTO

--- src/main/java/com/mysql/DEMO_MYSQL/dto/response/UserResponse.java
+++ src/main/java/com/mysql/DEMO_MYSQL/dto/response/UserResponse.java
@@ -1,5 +1,6 @@
 package com.mysql.DEMO_MYSQL.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 @Setter
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
     String id;
     String userName;
